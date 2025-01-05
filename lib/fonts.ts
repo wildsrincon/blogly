fd2bc772198780245b662fe756e51077d968224f
Wilds Rincon <wildsrincon.developer@gmail.com>
2025-01-05 10:20:25 -0500

    feat: Add navigation menu and card component shadcn
    Adding components navigation menu and card shadcn/ui

--- lib/fonts.ts
+++ lib/fonts.ts
@@ -1,6 +1,7 @@
 import { JetBrains_Mono as FontMono, Inter as FontSans } from "next/font/google"
 
 export const fontSans = FontSans({
+  weight: ["400", "500", "600", "700", "800", "900"],
   subsets: ["latin"],
   variable: "--font-sans",
 })
