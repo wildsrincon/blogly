5e9817500fedc81a24d1a7a082d95445de9b10e6
Wilds Rincon <wildsrincon.developer@gmail.com>
2025-01-04 21:13:22 -0500

    feat: first commit

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import { Card } from '@/components/ui/card'
+
+export default function IndexPage() {
+  return (
+    <section className="mx-auto mt-8 max-w-xl">
+      <Card className="p-4">
+        <h1 className='text-lg'>Hello world</h1>
+      </Card>
+    </section>
+  )
+}
